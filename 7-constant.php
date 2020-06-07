736a9954849e6fdca9fb48bac109899f10649669
kangbalja <kangbalja@gmail.com>
2020-06-07 22:01:59 +0900

    Add 7-constant

---
+++ 7-constant.php
@@ -0,0 +1,6 @@
+<?php
+    define("FAVORITE_DOLL", "gelatoni");
+    echo "상수 FAVORITE_DOLL의 값은 ".FAVORITE_DOLL."<br>";
+
+    define("FAVORITE_DOLL", "duffy");
+    echo "상수 FAVORITE_DOLL의 값은 ".FAVORITE_DOLL;
