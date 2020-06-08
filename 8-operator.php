ef94797555707a27c839ccc4da56923055fa8a7c
kangbalja <kangbalja@gmail.com>
2020-06-08 21:41:18 +0900

    Add 8-operator

---
+++ 8-operator.php
@@ -0,0 +1,15 @@
+<?php
+    $sum = 5 + 5;
+    echo "5 더하기 5 는 ".$sum;
+    echo "<br>";
+
+    $minus = 5 - 2;
+    echo "5 빼기 2 는 ".$minus;
+    echo "<br>";
+
+    $mul = 5 * 3;
+    echo "5 곱하기 3 은 ".$division;
+    echo "<br>";
+
+    $rest = 5 % 3;
+    echo "5 나누기 3의 나머지 값은 ".$rest;
