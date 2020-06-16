a2ce340cc697cf6f8d738e10d78ef4f978183107
kangbalja <kangbalja@gmail.com>
2020-06-16 23:28:13 +0900

    Add 9-operator

---
+++ 9-operator.php
@@ -0,0 +1,24 @@
+<?php
+    $num = 10;
+    $num += 2;
+    echo "[+=사용] 변수 num의 값은 ".$num."<br />";
+
+    $num = 10;
+    $num -= 2;
+    echo "[-=사용] 변수 num의 값은 ".$num."<br />";
+
+    $num = 10;
+    $num *= 2;
+    echo "[*=사용] 변수 num의 값은 ".$num."<br />";
+
+    $num = 10;
+    $num /= 2;
+    echo "[/=사용] 변수 num의 값은 ".$num."<br />";
+
+    $num = 10;
+    $num %= 3;
+    echo "[%=사용] 변수 num의 값은 ".$num."<br />";
+
+    $city = '서울';
+    $city .= '특별시';
+    echo "[.=사용] 변수 city의 값은 ".$city;
